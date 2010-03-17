070e860e8162e21519ca012fea6be2b2b0f3a0fd
r-a-y <r-a-y@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-03-17 18:14:26 +0000

    First version of oEmbed for Comments
    git-svn-id: https://plugins.svn.wordpress.org/oembed-for-comments/tags/0.6@218814 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ oembed-config.php
@@ -0,0 +1,10 @@
+<?php
+/*
+oEmbed for Comments settings
+*/
+
+// blacklist - skip urls from oEmbed parsing + caching
+// you can add sites you want to blacklist here
+// eg. uncomment the following line to blacklist links from youtube.com
+//$oembed_comments['blacklist'][] = 'youtube.com';
+?>
